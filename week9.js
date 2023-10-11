3626aad1c74a5fa18e0b90df173db5b2405e092c
makMonash <126857695+makMonash@users.noreply.github.com>
2023-10-12 02:03:06 +0800

    Update week9.js

--- week9.js
+++ week9.js
@@ -1,5 +1,52 @@
-var vg_1 = "hmw9.vg";
+var vg_1 = "Mapass2.vg";
 vegaEmbed("#map", vg_1).then(function(result) {
     // Access the Vega view instance 
     //(https://vega.github.io/vega/docs/api/view/) as result.view
   }).catch(console.error);
+
+  
+  var vg_5 = "ganttchart.vg";
+vegaEmbed("#gantt", vg_5).then(function(result) {
+    // Access the Vega view instance 
+    //(https://vega.github.io/vega/docs/api/view/) as result.view
+  }).catch(console.error);
+  
+  var vg_6 = "scatterplot.vg";
+vegaEmbed("#sp", vg_6).then(function(result) {
+    // Access the Vega view instance 
+    //(https://vega.github.io/vega/docs/api/view/) as result.view
+  }).catch(console.error);
+document.addEventListener("DOMContentLoaded", function () {
+  const chartSelector = document.getElementById("chartSelector");
+  const selectedChart = document.getElementById("selectedChart");
+chartSelector.value = "bar1";
+chartSelector.dispatchEvent(new Event("change"));
+
+  chartSelector.addEventListener("change", function () {
+      const selectedValue = chartSelector.value;
+      let vgFilePath;
+
+      switch (selectedValue) {
+
+          case "bar1":
+              vgFilePath = "barchart1.vg";
+              break;
+          case "bar2":
+              vgFilePath = "barchart2.vg";
+              break;
+          case "bar3":
+              vgFilePath = "barchart3.vg";
+              break;
+          default:
+              vgFilePath = null;
+      }
+
+      if (vgFilePath) {
+          vegaEmbed(selectedChart, vgFilePath).then(function (result) {
+              // Access the Vega view instance as result.view
+          }).catch(console.error);
+      } else {
+          selectedChart.innerHTML = "Select a chart to display.";
+      }
+  });
+});
